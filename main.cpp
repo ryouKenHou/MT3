b98cf152d59e26ca9e14a68668337646a5dd31e2
ryouKenHou <k025g0112@g.neec.ac.jp>
2026-05-28 15:09:54 +0900

    MT3_02_04: segment vs triangle

--- main.cpp
+++ main.cpp
@@ -14,6 +14,11 @@ static const int kRowHeight = 20;
 // struct 
 // =======================================================================
 
+struct Vector2i {
+	int x;
+	int y;
+};
+
 struct Sphere {
 	Vector3 center;
 	float radius;
@@ -37,6 +42,10 @@ struct Segment {
 struct Plane{
 	Vector3 normal;
 	float distance;
+};
+
+struct Triangle {
+	Vector3 vertices[3];
 };
 
 // ==============================================================================
@@ -152,6 +161,18 @@ void DrawPlane(const Plane& plane, const Matrix4x4& viewProjectionMatrix, const 
 
 }
 
+void DrawTriangle(const Triangle& triangle, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color) {
+	Vector3 screenVertices[3];
+	for (int i = 0; i < 3; ++i) {
+		screenVertices[i] = Transform(triangle.vertices[i], viewProjectionMatrix * viewportMatrix);
+	}
+	Novice::DrawTriangle(
+		static_cast<int>(screenVertices[0].x), static_cast<int>(screenVertices[0].y),
+		static_cast<int>(screenVertices[1].x), static_cast<int>(screenVertices[1].y),
+		static_cast<int>(screenVertices[2].x), static_cast<int>(screenVertices[2].y), color, kFillModeWireFrame
+	);
+}
+
 Vector3 Project(const Vector3& v1, const Vector3& v2) {
 	Vector3 normalizedV2 = v2.Normalized();
 	float dotProduct = v1.Dot(normalizedV2);
@@ -192,6 +213,23 @@ bool isCollision(const Segment& segment, const Plane& plane) {
 	return (distanceOrigin * distanceEnd <= 0.f);
 }
 
+bool isCollision(const Triangle& triangle, const Segment& segment) {
+	Vector3 edge1 = triangle.vertices[1] - triangle.vertices[0];
+	Vector3 edge2 = triangle.vertices[2] - triangle.vertices[0];
+	Vector3 planeNormal = Vector3::Cross(edge1, edge2).Normalized();
+	Plane plane{ planeNormal, planeNormal.Dot(triangle.vertices[0]) };
+	if (!isCollision(segment, plane)) {
+		return false;
+	}
+	Vector3 intersectionPoint = segment.origin + segment.diff * ((plane.distance - segment.origin.Dot(plane.normal)) / segment.diff.Dot(plane.normal));
+	Vector3 c0 = intersectionPoint - triangle.vertices[0];
+	Vector3 c1 = intersectionPoint - triangle.vertices[1];
+	Vector3 c2 = intersectionPoint - triangle.vertices[2];
+	return Vector3::Cross(edge1, c0).Dot(planeNormal) >= 0.f &&
+		Vector3::Cross(triangle.vertices[2] - triangle.vertices[1], c1).Dot(planeNormal) >= 0.f &&
+		Vector3::Cross(triangle.vertices[0] - triangle.vertices[2], c2).Dot(planeNormal) >= 0.f;
+}
+
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
@@ -201,13 +239,16 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
+	Vector2i mousePos = { 0, 0 };
+	Vector2i preMousePos = { 0, 0 };
+	int mouseWheel = 0;
 
 	Vector3 cameraPos = { 0.f, 1.9f, -6.49f };
 	Vector3 cameraRotate = { 0.26f, 0.f, 0.f };
 
 	Vector3 pos1 = { 0, 0, 0 };
 
-	Plane plane1{ Vector3(0, 1, 0), 0 };
+	Triangle triangle1{ { Vector3(0, 0, 0), Vector3(1, 0, 0), Vector3(0, 1, 0) } };
 	Segment segment1{ Vector3(-1, 0, 0), Vector3(2, 0, 0) };
 
 	// ウィンドウの×ボタンが押されるまでループ
@@ -219,25 +260,47 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		memcpy(preKeys, keys, 256);
 		Novice::GetHitKeyStateAll(keys);
 
+		preMousePos = mousePos;
+		Novice::GetMousePosition(&mousePos.x, &mousePos.y);
+
+		
+		mouseWheel = Novice::GetWheel();
+
 		///
 		/// ↓更新処理ここから
 		///
+		
+		if (Novice::IsPressMouse(1)) {
+			cameraRotate.y += (mousePos.x - preMousePos.x) * 0.01f;
+			cameraRotate.x += (mousePos.y - preMousePos.y) * 0.01f;
+		}
+
+		if (mouseWheel != 0) {
+			Vector3 cameraDirection;
+			cameraDirection.x = cosf(cameraRotate.x) * sinf(cameraRotate.y);
+			cameraDirection.y = sinf(cameraRotate.x);
+			cameraDirection.z = cosf(cameraRotate.x) * cosf(cameraRotate.y);
+			cameraDirection = cameraDirection.Normalized();
+			cameraPos = cameraPos + cameraDirection * float(mouseWheel) * 0.01f;
+		}
+
+
+
 		ImGui::Begin("window");
 		ImGui::DragFloat3("cameraPos", &cameraPos.x, 0.1f);
 		ImGui::DragFloat3("cameraRotate", &cameraRotate.x, 0.01f);
 		ImGui::DragFloat3("segment1Origin", &segment1.origin.x, 0.1f);
 		ImGui::DragFloat3("segment1Diff", &segment1.diff.x, 0.1f);
-		ImGui::DragFloat("plane1Distance", &plane1.distance, 0.1f);
-		ImGui::DragFloat3("plane1Normal", &plane1.normal.x, 0.1f);
+		ImGui::DragFloat3("triangle1Vertex0", &triangle1.vertices[0].x, 0.1f);
+		ImGui::DragFloat3("triangle1Vertex1", &triangle1.vertices[1].x, 0.1f);
+		ImGui::DragFloat3("triangle1Vertex2", &triangle1.vertices[2].x, 0.1f);
 		ImGui::End();
-		plane1.normal = plane1.normal.Normalized();
-
 		Matrix4x4 cameraMatrix = Matrix4x4::MakeAffineMatrix(Vector3(1, 1, 1), cameraRotate, cameraPos);
 		Matrix4x4 viewMatrix = Matrix4x4::Inverse(cameraMatrix);
 		Matrix4x4 projectionMatrix = Matrix4x4::MakePerspectiveFovMatrix(0.45f, 1280.0f / 720.0f, 0.1f, 100.0f);
 		Matrix4x4 viewPortMatrix = Matrix4x4::MakeViewportMatrix(0, 0, 1280, 720, 0.0f, 1.0f);
 
-		bool collision = isCollision(segment1, plane1);
+		bool collision = isCollision(triangle1, segment1);
 
 		///
 		/// ↑更新処理ここまで
@@ -250,7 +313,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		Vector3 segmentEnd = Transform(segment1.origin + segment1.diff, viewMatrix * projectionMatrix * viewPortMatrix);
 		Novice::DrawLine(static_cast<int>(segmentStart.x), static_cast<int>(segmentStart.y), static_cast<int>(segmentEnd.x), static_cast<int>(segmentEnd.y), collision ? 0xFF0000FF : 0xFFFFFFFF);
 
-		DrawPlane(plane1, viewMatrix * projectionMatrix, viewPortMatrix, collision ? 0xFF0000FF : 0xFFFFFFFF);
+		DrawTriangle(triangle1, viewMatrix * projectionMatrix, viewPortMatrix, collision ? 0xFF0000FF : 0xFFFFFFFF);
+	
 
 		//DrawSphere(sphere1, viewMatrix * projectionMatrix, viewPortMatrix, collision ? 0xFF0000FF : 0xFFFFFFFF);
 		
